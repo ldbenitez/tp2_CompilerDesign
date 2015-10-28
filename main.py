077f05a9a76345bb6c8f082d23a766c194cee147
cesarrecalde <ce.sarrecalde@hotmail.com>
2015-10-28 11:49:47 -0300

    Conjunto primero y siguiente terminado

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
 #-*- coding: utf-8 -*-
 
-from Util import Grammar,Production
+from Grammar import Grammar
+
 
 def main():
     grammar = Grammar()
     production = None
     input_list = []
 
-    # print "Write the Grammar:"
-    # while True:
-    #     input_str = raw_input(">")
-    #     if input_str == ".":
-    #         break
-    #     else:
-    #         input_list.append(input_str)
+    grammar.init_grammar("grammar_input_example.txt")
+    print "Non Terminals: " + grammar.non_terminals.__str__()
+    print "Terminals:" + grammar.terminals.__str__()
 
-    with open("grammar_input_example.txt",'r') as grammar_input:
-        for line in grammar_input:
-            tmp_split = line.rsplit("->",1)
-            left_part = tmp_split[0]
-            right_part = tmp_split[1].rsplit("|",1)
-            grammar.non_terminals.append(left_part.strip())
-            for item in right_part:
-                production = Production()
-                each_right_part = item.split(",")
-                for subitem in each_right_part:
-                    production.body.append(subitem.replace("\n","").strip())
-                    production.name = left_part.strip()
-                grammar.productions.append(production)
+    grammar.define_first_sets()
+    grammar.define_follow_sets()
 
-        print grammar.non_terminals
-        print grammar.productions[3].body
+    print "First Set: \n\t" + grammar.get_first_sets().__str__()
+    print "Follow Set: \n\t" + grammar.get_follow_sets().__str__()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
